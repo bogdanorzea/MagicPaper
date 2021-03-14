77d000c1e16423ef7ec2d1fe04e37b10ba077211
Bogdan Orzea <bogdan.orzea@gmail.com>
2021-03-13 19:57:14 -0500

    Complete Section 30

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -21,12 +21,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +28,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "Newspaper Images", bundle: .main) {
+            configuration.detectionImages = trackedImages
+            configuration.maximumNumberOfTrackedImages = 1
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,28 +45,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
+
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+
+        if let imageAnchor = anchor as? ARImageAnchor {
+            // Create a SpriteKit scene
+            let videoScene = SKScene(size: CGSize(width: 1280.0, height: 720.0))
+            // Create a SpriteKit VideoNode
+            let videoNode = SKVideoNode(fileNamed: "harrypotter.mp4")
+            videoNode.play()
+            // Flip the video
+            videoNode.yScale = -1.0
+            // Center the video into the scene
+            videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+            videoScene.addChild(videoNode)
+
+            let plane = SCNPlane(
+                width: imageAnchor.referenceImage.physicalSize.width,
+                height: imageAnchor.referenceImage.physicalSize.height
+            )
+
+            // Adds VideoScene as the material of the plane
+            plane.firstMaterial?.diffuse.contents = videoScene
+
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -Float.pi / 2
+
+            node.addChildNode(planeNode)
+        }
+
         return node
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
+
 }
